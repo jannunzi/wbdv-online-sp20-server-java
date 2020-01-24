5c1f23eb190ad9c45cf495d985cd7929f5d820d4
jannunzi <jannunzi@gmail.com>
2020-01-23 21:31:03 -0500

    added user client service, template, and controller

--- src/main/webapp/user-admin/user-admin.js
+++ src/main/webapp/user-admin/user-admin.js
@@ -1,41 +1,99 @@
-let h1 = $("h1")
-h1.html("User Admin Screen!!!")
-    .css("color", "green")
+// IIFE
+// Immediately Invoked Function Expressions
+(function () {
+    let userService = new AdminUserServiceClient()
+
+    let h1 = $("h1")
+    h1.html("User Admin Screen!!!")
+        .css("color", "green")
     // .remove()
 
-let users = [
-    "alice", "bob"
-];
-let userList = $("#user-list")
-let usernameFld = $("#username-fld")
+    let users = [];
+    let userList = $("#user-list")
+    let usernameFld = $("#username-fld")
 
+    const deleteUser = (index) => {
 
-const deleteUser = (index) => {
-    users.splice(index, 1)
-    renderUsers()
-}
+        const userId = users[index]._id;
+        userService.deleteUser(userId)
+            .then(response => {
+                users.splice(index, 1)
+                renderUsers()
+                // findAllUsers()
+            })
 
-const renderUsers = () => {
-    userList.empty()
-    for (let u = 0; u < users.length; u++) {
-        const li = $("<li>")
-        // li.html(users[u])
-        const deleteBtn = $("<button>Delete</button>")
-        deleteBtn.click(() => {deleteUser(u)})
-        li.append(deleteBtn)
-        li.append(users[u])
-        userList.append(li)
     }
-}
-renderUsers()
+    let currentUserId = -1
+    const editUser = index => {
+        const userId = users[index]._id;
+        currentUserId = userId;
+        userService.findUserById(userId)
+            .then(user => {
+                console.log(user)
+                usernameFld.val(user.username)
+            })
+    }
 
-const createBtn = $("#create-btn");
+    const renderUsers = () => {
+        userList.empty()
+        for (let u = 0; u < users.length; u++) {
+            const li = $("<li>")
+            // li.html(users[u])
+            const editBtn = $("<button>Edit</button>")
+            editBtn.click(() => {
+                editUser(u)
+            })
 
-const createUser = () => {
-    const username = usernameFld.val()
-    usernameFld.val("")
-    users.push(username)
+            const deleteBtn = $("<button>Delete</button>")
+            deleteBtn.click(() => {
+                deleteUser(u)
+            })
+            li.append(deleteBtn)
+            li.append(editBtn)
+            li.append(users[u].username)
+            userList.append(li)
+        }
+    }
     renderUsers()
-}
 
-createBtn.click(createUser)
+    const updateUser = () => {
+        const username = usernameFld.val()
+        usernameFld.val("")
+
+        userService.updateUser(currentUserId, {username: username})
+            .then(newUser => {
+                // users.push(newUser)
+                // renderUsers()
+                findAllUsers()
+            })
+
+    }
+
+    const createBtn = $("#create-btn");
+    const updateBtn = $("#update-btn");
+
+    updateBtn.click(updateUser)
+
+    const createUser = () => {
+        const username = usernameFld.val()
+        usernameFld.val("")
+
+        userService.createUser({username: username})
+            .then(newUser => {
+                // users.push(newUser)
+                // renderUsers()
+                findAllUsers()
+            })
+    }
+
+    createBtn.click(createUser)
+
+    const findAllUsers = () =>
+        userService.findAllUsers()
+            .then((theUsers) => {
+                users = theUsers
+                renderUsers()
+        })
+
+    findAllUsers()
+})();
